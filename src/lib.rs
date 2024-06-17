6b85a740e777dca31d5327b753d81ba1ab1c7729
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2024-06-17 17:08:44 +0700

    Add a first test for the initial function provided by maturin

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 use pyo3::prelude::*;
 
+#[cfg(test)]
+mod tests {
+    use super::*; // bring the module under test into scope
+
+    #[test]
+    fn it_works() {
+        let result: usize = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+fn add(a: usize, b: usize) -> usize {
+    a + b
+}
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+    Ok((add(a, b)).to_string())
 }
 
 /// A Python module implemented in Rust.
